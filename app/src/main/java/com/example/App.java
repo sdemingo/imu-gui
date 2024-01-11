08f569f764625f91fba607827a2023dbf67845a6
sergio <sdemingo@gmail.com>
2024-01-11 12:52:44 +0100

    añado modificacion de las label desde hilo

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -5,39 +5,55 @@ import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.scene.*;
+import java.util.Random;
+import javafx.application.Platform;
+
 
 public class App extends Application {
 
     SerialMonitor serial;
 
     @Override
-    public void start(Stage primera) throws Exception {
-        primera.setTitle("Detección de movimiento");
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Detección de movimiento");
 
-        HBox root =new HBox();
+        HBox root =new HBox(10);
         Label anguloX = new Label("0");
         Label anguloY = new Label("0");
         root.getChildren().addAll(anguloX,anguloY);
+        
+        stage.setScene(new Scene(root, 300, 100));
 
+        // separate non-FX thread
+        new Thread() {
+            // Can I kill the thread?
+            public void run() {
+                for (;;){
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    final int randValue1 = new Random().nextInt(90);
+                    final int randValue2 = new Random().nextInt(90);
+        
+                    Platform.runLater(new Runnable() {
+                            public void run() {
+                                anguloX.setText(Integer.toString(randValue1));
+                                anguloY.setText(Integer.toString(randValue2));
+                            }
+                        });
+                }
+            }
+        }.start();        
 
-        Scene escena = new Scene(root,500,300);
-        primera.setScene(escena);
-
-        primera.show();
+        stage.show();
     }
 
 
-    /*
-      Tengo que meter en el stage dos labels e ir actualizandolas con los 
-      valores que están en el buffer de serialmonitor
-
-      https://stackoverflow.com/questions/29358075/running-a-thread-in-javafx
-     */
-
-
     public static void main(String[] args) {
-        serial = new SerialMonitor();
-        serial.init();
+        //serial = new SerialMonitor();
+        //serial.init();
         launch();
     }
 }
